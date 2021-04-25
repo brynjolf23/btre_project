c2196bfa2135486a135ce066f9e66b557d1dbfc0
brynjolf23 <saleemalii@live.com>
2021-04-25 18:26:00 -0400

    removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -144,15 +144,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-
-# email from django.conf import settings
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brynjolf23@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-
-
 try:
     from .local_settings import *
 except ImportError:
