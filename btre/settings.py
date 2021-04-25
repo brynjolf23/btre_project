928483ec93d73ce6defe8212c5dc1cc30c685939
brynjolf23 <saleemalii@live.com>
2021-04-25 18:20:20 -0400

    added local setting import

--- btre/settings.py
+++ btre/settings.py
@@ -151,3 +151,9 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'brynjolf23@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
